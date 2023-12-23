c3d217d31dd2167040e339ab7db98db98f262fe4
Vuong Nguyen <nguvuong@sheridancollege.ca>
2023-12-23 16:50:57 -0500

    created file and folder structure

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 // box is a div with shading and colors 
 import {Box} from '@mui/material'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <Box width="400px">
-        NavBar
+    // m = "auto" is margin auto
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto ">
+        <NavBar/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+
+        </Routes>
+        <Footer/>
     </Box>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import App from "./App.js"
-
+import { BrowserRouter } from "react-router-dom";
 
 const root =  ReactDOM.createRoot(document.getElementById('root'));
 
 
-root.render(<App/>)
+root.render(
+
+    //   use browser router for functioning of the routes 
+<BrowserRouter>
+<App/>
+</BrowserRouter>
+);

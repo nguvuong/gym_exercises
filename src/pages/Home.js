4ee07154ff9cdcb5374be0bf9261543abead7e41
Vuong Nguyen <nguvuong@sheridancollege.ca>
2023-12-24 01:00:56 -0500

    finished designing the hero banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Box from '@mui/material';
+import Box from '@mui/material/Box';
 import {Link} from 'react-router-dom'
 import {Stack} from '@mui/material'
 import HeroBanner from '../components/HeroBanner';

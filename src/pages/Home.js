f05c97bff1d00fb6692fc13a0d062bb4e54bf5e8
Vuong Nguyen <nguvuong@sheridancollege.ca>
2023-12-24 00:30:38 -0500

    finish designing the navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Box from '@mui/material';
+import {Link} from 'react-router-dom'
+import {Stack} from '@mui/material'
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+
+      {/* the part with the text */}
+      <HeroBanner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
   )
 }
 
